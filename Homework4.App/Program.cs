ce5b3ddaf540efb2b5756c34c934ea1737b62fc9
aburlaka2 <aburlaka1@gmail.com>
2020-11-20 19:53:58 +0200

    Update Program.cs

--- Homework4.App/Program.cs
+++ Homework4.App/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp3
         static void Main()
         {
             bool repeat = true;
-            do
+            while (repeat)
             {
                 int choice = Choice();
                 if (choice == 1)
@@ -30,8 +30,8 @@ namespace ConsoleApp3
                 {
                     Transponention();
                 }
-                Repeat(repeat);
-            } while (repeat);
+                repeat=Repeat();
+            }
             
             
 
@@ -39,9 +39,10 @@ namespace ConsoleApp3
     
         static int Choice()
         {
-            int choice = 0;
+            int choice;
             do
             {
+                Console.Clear();
                 Console.WriteLine("Эта программа позволяет производить операции над матрицами");
                 Console.WriteLine("Какую операцию вы хотите осущесвить?");
                 Console.WriteLine("");
@@ -98,7 +99,6 @@ namespace ConsoleApp3
                     continue;
                 }
             }
-            Console.Clear();
             return number;
         }
         static void WriteMatrix(int rows, int columns, int[,] matrixresult)
@@ -115,33 +115,31 @@ namespace ConsoleApp3
         }
         static void MatrixSum()
         {
+            Console.Clear();
             Console.WriteLine("Введите колличество строк в первой матрице");
-            int rows1 = Size();
+            int rows = Size();
             Console.WriteLine("Введите колличество столбцов в первой матрице");
-            int columns1 = Size();
-            int[,] matrix1 = new int[rows1, columns1];
-            for (int i = 0; rows1 > i; i++)
+            int columns = Size();
+            int[,] matrix1 = new int[rows, columns];
+            for (int i = 0; rows > i; i++)
             {
-                for (int j = 0; columns1 > j; j++)
+                for (int j = 0; columns > j; j++)
                 {
                     Console.WriteLine("Введите значение элемента первой матрицы " + (i + 1) + "," + (j + 1));
                     matrix1[i, j] = NumberInput();
                 }
             }
+            Console.Clear();
             Console.WriteLine("Вторая матрица будет иметь такую же размерность, как и первая, иначе сложение невозможно");
-            int rows2 = rows1;
-            int columns2 = rows1;
-            int[,] matrix2 = new int[rows2, columns2];
-            for (int i = 0; rows2 > i; i++)
+            int[,] matrix2 = new int[rows, columns];
+            for (int i = 0; rows > i; i++)
             {
-                for (int j = 0; columns2 > j; j++)
+                for (int j = 0; columns > j; j++)
                 {
-                    Console.WriteLine("Введите значение элемента первой матрицы " + (i + 1) + "," + (j + 1));
+                    Console.WriteLine("Введите значение элемента второй матрицы " + (i + 1) + "," + (j + 1));
                     matrix2[i, j] = NumberInput();
                 }
             }
-            int rows = rows1;
-            int columns = columns1;
             int[,] matrixresult = new int[rows, columns];
             for (int i = 0; rows > i; i++)
             {
@@ -150,10 +148,12 @@ namespace ConsoleApp3
                     matrixresult[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
             }
+            Console.Clear();
             WriteMatrix(rows, columns, matrixresult);
         }
         static void MatrixOnNumber()
         {
+            Console.Clear();
             Console.WriteLine("Введите колличество строк в матрице");
             int rows1 = Size();
             Console.WriteLine("Введите колличество столбцов в матрице");
@@ -167,6 +167,7 @@ namespace ConsoleApp3
                     matrix1[i, j] = NumberInput();
                 }
             }
+            Console.Clear();
             Console.WriteLine("Введите число, на которое нужно умножить матрицу");
             int number = NumberInput();
             int rows = rows1;
@@ -179,10 +180,12 @@ namespace ConsoleApp3
                     matrixresult[i, j] = matrix1[i, j] *number;
                 }
             }
+            Console.Clear();
             WriteMatrix(rows, columns, matrixresult);
         }
         static void MatrixOnMatrix()
         {
+            Console.Clear();
             Console.WriteLine("Введите колличество строк в первой матрице");
             int rows1 = Size();
             Console.WriteLine("Введите колличество столбцов в первой матрице");
@@ -196,10 +199,11 @@ namespace ConsoleApp3
                     matrix1[i, j] = NumberInput();
                 }
             }
-            Console.WriteLine("Вторая матрица будет иметь столько же столбцов, сколько строк в первой");
-            int columns2 = rows1;
+            Console.Clear();
             Console.WriteLine("Введите колличество строк во второй матрице");
             int rows2 = Size();
+            Console.WriteLine("Введите колличество столбцов во второй матрице");
+            int columns2 = Size();
             int[,] matrix2 = new int[rows2, columns2];
             for (int i = 0; rows2 > i; i++)
             {
@@ -209,8 +213,35 @@ namespace ConsoleApp3
                     matrix2[i, j] = NumberInput();
                 }
             }
-            int rows = rows2;
-            int columns = columns1;
+            if (rows1 == columns2 && rows2 != columns1)
+            {
+                Console.Clear();
+                Console.WriteLine("Возможно умножение первой матрицы на вторую");
+                Console.WriteLine("Результат");
+                Matrix1OnMatrix2(rows1, rows2, columns2, matrix1, matrix2);
+            }
+            else if (rows1 != columns2 && rows2 == columns1)
+            {
+                Console.Clear();
+                Console.WriteLine("Возможно умножение второй матрицы на первую");
+                Console.WriteLine("Результат");
+                Matrix2OnMatrix1(rows1, columns1, rows2, matrix1, matrix2);
+            }
+            else if (rows1 == columns2 && rows2 == columns1)
+            {
+                Console.Clear();
+                Console.WriteLine("Возможы два варианта умножения");
+                Console.WriteLine("Результат умножения первой матрицы на вторую");
+                Matrix1OnMatrix2(rows1, rows2, columns2, matrix1, matrix2);
+                Console.WriteLine("Результат умножения второй матрицы на первую");
+                Matrix2OnMatrix1(rows1, columns1, rows2, matrix1, matrix2);
+            }
+            else Console.WriteLine("Умножение матриц невозможно");
+        }
+        static void Matrix1OnMatrix2(int rows1, int rows2, int columns2,int[,]matrix1, int[,] matrix2)
+        {
+            int rows = rows1;
+            int columns = columns2;
             int[,] matrixresult = new int[rows, columns];
             for (int i = 0; rows > i; i++)
             {
@@ -218,7 +249,24 @@ namespace ConsoleApp3
                 {
                     for (int k = 0; rows2 > k; k++)
                     {
-                        matrixresult[i, j] = matrixresult[i, j] + matrix1[k, i] * matrix2[j, k];
+                        matrixresult[i, j] = matrixresult[i, j] + matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+            WriteMatrix(rows, columns, matrixresult);
+        }
+        static void Matrix2OnMatrix1(int rows1, int columns1, int rows2, int[,] matrix1, int[,] matrix2)
+        {
+            int rows = rows2;
+            int columns = columns1;
+            int[,] matrixresult = new int[rows, columns];
+            for (int i = 0; rows > i; i++)
+            {
+                for (int j = 0; columns > j; j++)
+                {
+                    for (int k = 0; rows1 > k; k++)
+                    {
+                        matrixresult[i, j] = matrixresult[i, j] + matrix2[i, k] * matrix1[k, j];
                     }
                 }
             }
@@ -244,7 +292,8 @@ namespace ConsoleApp3
             int rows = rows1;
             int columns = columns1;
             int[,] matrixresult = new int[rows, columns];
-            for(int n=0; exponent > n; n++)
+            int[,] matrixresult2 = new int[rows, columns];
+            for (int n=1; exponent > n; n++)
             {
                 for (int i = 0; rows > i; i++)
                 {
@@ -252,16 +301,24 @@ namespace ConsoleApp3
                     {
                         for (int k = 0; rows1 > k; k++)
                         {
-                            matrixresult[i, j] = matrixresult[i, k] * matrix1[k, j];
+                            if (n == 1)
+                            {
+                                matrixresult[i, k] = matrix1[i,k];
+                            }
+                            matrixresult2[i, j] = matrixresult2[i, j] + matrixresult[i, k] * matrix1[k, j];
                         }
+                        matrixresult[i, j] = matrixresult2[i, j];
+
                     }
+
                 }
             }
-            
-            WriteMatrix(rows, columns, matrixresult);
+            Console.Clear();
+            WriteMatrix(rows, columns, matrixresult2);
         }
         static void Transponention()
         {
+            Console.Clear();
             Console.WriteLine("Введите колличество строк в матрице");
             int rows1 = Size();
             Console.WriteLine("Введите колличество столбцов в матрице");
@@ -286,11 +343,13 @@ namespace ConsoleApp3
                     matrixresult[i, j] = matrix1[j, i];
                 }
             }
+            Console.Clear();
             WriteMatrix(rows, columns, matrixresult);
         }
-        static bool Repeat(bool repeat)
+        static bool Repeat()
         {
-            bool repeat2 = true;
+            bool repeat;
+            bool repeat2;
             do
             {
                 Console.WriteLine("Хотите ли продолжить работу с программой? д/н");
@@ -308,6 +367,7 @@ namespace ConsoleApp3
                 else
                 {
                     Console.WriteLine("Ответьте 'д' или 'н'");
+                    repeat = true;
                     repeat2 = true;
                 }
             }
